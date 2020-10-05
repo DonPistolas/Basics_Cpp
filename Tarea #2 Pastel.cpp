e9b8e659d3679238a349de48b15638f242c70d11
DonPistolas <69009964+DonPistolas@users.noreply.github.com>
2020-10-05 11:02:01 -0500

    Tarjeta de cumpleaños 1.0
    Ingresa tu año de nacimiento y sopla las velas.

---
+++ Tarea #2 Pastel.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	int date;
+	char nam[20];
+	cout<<"Cual es tu nombre?\n"; cin>>nam;
+	
+	cout<<"Cual es tu anio de nacimiento?\n"; cin>>date;
+	
+	cout<<"    ||||||\n";
+	  cout<<"  @@@@@@@@@@\n";
+	  cout<<"   {@-@-@-@}\n";
+	  cout<<"@@@@@@@@@@@@@@@\n";
+	  cout<<"{-@  Happy  @-}\n";
+	  cout<<"{ Birthday #";cout<<2020-date; cout<<"}\n";
+	  cout<<"{    "; cout<<nam; cout<<"  }\n"; 
+	  cout<<"@@@@@@@@@@@@@@@@@\n";
+} /* No se como establecer el programa para reiniciarse */
